e758534da9c6386f12fe17a869caac4daf2cddee
Joshua Tan <joshuatanjc04@gmail.com>
2026-08-20 17:39:23 +0800

    Added greeting

--- src/main/java/Pathfinder.java
+++ src/main/java/Pathfinder.java
@@ -3,6 +3,14 @@ public class Pathfinder {
         String banner = "/================\\\n"
                 + "|   Pathfinder   |\n"
                 + "\\================/\n";
-        System.out.println(banner);
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello friend! My name is Pathfinder.");
+        System.out.println("What tasks can I do for you today?");
+        System.out.println(separator);
+        System.out.println("Bye bye! Hope to see you around soon!");
+        System.out.println(separator);
+
     }
 }
