697b12082e0464c0df6837c62f1d6e83d935e23e
Joshua Tan <joshuatanjc04@gmail.com>
2026-08-20 17:21:48 +0800

    Renamed chatbot

---
+++ src/main/java/Pathfinder.java
@@ -0,0 +1,8 @@
+public class Pathfinder {
+    public static void main(String[] args) {
+        String banner = "/================\\\n"
+                + "|   Pathfinder   |\n"
+                + "\\================/\n";
+        System.out.println(banner);
+    }
+}
