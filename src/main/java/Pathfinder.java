56a4593b00a5fe8d3e145ca048ff68a4f205b040
Joshua Tan <joshuatanjc04@gmail.com>
2026-08-20 19:08:02 +0800

    Added TaskList class

--- src/main/java/Pathfinder.java
+++ src/main/java/Pathfinder.java
@@ -4,19 +4,22 @@ public class Pathfinder {
     private static final String separator = "____________________________________________________________";
 
     public static void main(String[] args) {
-        String banner = "/================\\\n"
-                + "|   Pathfinder   |\n"
-                + "\\================/\n";
-        System.out.println(separator);
-        System.out.print(banner);
-        System.out.println("Hello friend! My name is Pathfinder.");
-        System.out.println("What tasks can I do for you today?");
-        System.out.println(separator);
+        greetMessage();
         Scanner scanner = new Scanner(System.in);
         String msg = scanner.nextLine();
-        while (!msg.equals("bye")) {
-            echoMessage(msg);
+        TaskList task = new TaskList();
+        while (true) {
+            if (msg.equals("bye")) {
+                break;
+            }
+            if (msg.equals("list")) {
+                task.printList();
+                msg = scanner.nextLine();
+                continue;
+            }
+            task.addTask(msg);
             msg = scanner.nextLine();
+
         }
         scanner.close();
         System.out.println("Bye bye! Hope to see you around soon!");
@@ -28,6 +31,17 @@ public class Pathfinder {
     public static void echoMessage(String message) {
         System.out.println(separator);
         System.out.println(message);
+        System.out.println(separator);
+    }
+
+    public static void greetMessage() {
+        String banner = "/================\\\n"
+                + "|   Pathfinder   |\n"
+                + "\\================/\n";
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello friend! My name is Pathfinder.");
+        System.out.println("What tasks can I do for you today?");
         System.out.println(separator);
     }
 }
